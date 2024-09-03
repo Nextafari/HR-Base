c610be4ec3c3247427303cb18022847b996bc972
Nextafari <chinedue16@gmail.com>
2024-09-03 16:23:42 +0100

    Update: modified settings.py to include Allowed_Hosts when Debug is set to False

--- hr_base/settings.py
+++ hr_base/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "yes").lower() in ("yes", "true")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
 
 
 # Application definition
